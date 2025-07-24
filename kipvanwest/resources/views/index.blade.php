8bb51225c8dc034290cde04649f0b4e397e89c82
ThijnGlas <thijnglas@gmail.com>
2025-07-24 14:46:35 +0200

    first commit

---
+++ kipvanwest/resources/views/index.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kip van West</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+
+<body>
+    <header>
+        <div class="navigation-sticky">
+            <div class="navigation-wrapper">
+                <nav class="navigation">
+                    <img src="{{ asset('images/logo.png') }}" alt="">
+                    <ul>
+                        <li><a href="">Over ons</a></li>
+                        <li><a href="">Onze productlijst</a></li>
+                        <li><a href="">Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
+
+        </div>
+        <div class="header-content">
+            <h1>Kip van West</h1>
+            <a class="btn" href="">Bekijk onze prijslijst</a>
+        </div>
+
+    </header>
+    <main>
+        <div class="subtitles">
+            <ul>
+                <li>Maaltijden</li>
+                <li>Gegrilde kip</li>
+                <li>luxe rundvlees</li>
+                <li>Heerlijke kipspecialiteiten</li>
+            </ul>
+        </div>
+        <div class="main-content">
+
+        </div>
+    </main>
+</body>
+
+</html>
